67a0f45509650198858d063ee01bb1657eef8147
gloom_04 <116475475+flo3r04@users.noreply.github.com>
2024-07-22 17:16:05 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	ios::sync_with_stdio(false);
+
+	unsigned int n;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+
+	{
+
+		cout << i << "\n";
+
+	}
+
+	return 0;
+
+}
