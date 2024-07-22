3b9acedd394191d5ff509698a57f1b486a478255
gloom_04 <116475475+flo3r04@users.noreply.github.com>
2024-07-22 17:22:49 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    int a, b;
+
+    cin >> n;
+
+    while(n>0) {
+
+        cin >> a >> b;
+
+        cout << a+b << endl;
+
+        n--;
+
+        }
+
+    return 0;
+
+}
