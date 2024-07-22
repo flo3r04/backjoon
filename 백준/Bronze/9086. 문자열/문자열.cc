fcd77b051d4627c064c97ccd6a0368e9d6f68e00
gloom_04 <116475475+flo3r04@users.noreply.github.com>
2024-07-22 17:20:36 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    for(int i=0; i < n; i++) {
+
+        string str;
+
+        cin >> str;
+
+        cout << str.front() << str.back() << endl;
+
+        }
+
+}
+
+        
